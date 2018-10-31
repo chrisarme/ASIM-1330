68ba4a54a4e500d3f1f42fc468e7d7bac687cc71
Chris Arme <christopher.arme@gmail.com>
2018-10-31 00:46:12 -0500

    ASIM Project 2 work

---
+++ ASIM Project 2/A Future World/A Future World/Assets/Scripts/SpeechBubble.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble : MonoBehaviour {
+
+    public string personText;
+
+    public GameObject speechBubble;
+    GameObject instBubble;
+
+    Renderer rend;
+
+	// Use this for initialization
+	void Start ()
+    {
+        instBubble = Instantiate(speechBubble, transform.position + new Vector3(0, 2, 0), Quaternion.identity) as GameObject;
+
+        rend = instBubble.GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float dist = Vector3.Distance(instBubble.transform.position, Camera.main.transform.position);
+        //Debug.Log(dist);
+
+        if (dist < 10)
+        {
+            rend.material.color = new Color(rend.material.color.r, rend.material.color.b, rend.material.color.g, 8 - dist);
+        }
+        else
+        {
+            rend.material.color = new Color(rend.material.color.r, rend.material.color.b, rend.material.color.g, 0);
+        }
+
+    }
+}
---
+++ ASIM Project 2/A Future World/A Future World/Assets/Scripts/TextScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextScript : MonoBehaviour
+{
+    TextMeshPro textMeshPro;
+
+    // Use this for initialization
+    void Start()
+    {
+        textMeshPro = GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 v = Camera.main.transform.position - transform.position;
+        //v.x = v.z = 0.0f;
+        transform.LookAt(Camera.main.transform.position);
+        transform.Rotate(0, 180, 0);
+
+        float dist = Vector3.Distance(transform.position, Camera.main.transform.position);
+        //Debug.Log(dist);
+
+        if (dist < 10)
+        {
+            textMeshPro.color = new Color(textMeshPro.color.r, textMeshPro.color.b, textMeshPro.color.g, 8 - dist);
+        }
+        else
+        {
+            textMeshPro.color = new Color(textMeshPro.color.r, textMeshPro.color.b, textMeshPro.color.g, 0);
+        }
+
+        //transform.position = Camera.main.WorldToScreenPoint(transform.position);
+    }
+}
